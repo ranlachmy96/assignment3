e8afe7c4a6b976a8b709bd30f1cbd16e1d0241e2
Ran Lachmy <ranlachmy96@gmail.com>
2024-03-02 16:33:19 +0200

    added main page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import FamilyPicture from './assets/familypicture.png';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div id="mainPageContainer">
+            <div id="mainPageMiddlePartContainer">
+                <div id="mainPageTitlesContainer">
+                    <h5>Healing Hearts, Healing Lives</h5>
+                    <h1>Being Part Of Family Reunification Is A Way To Help</h1>
+                </div>
+                <div id="upperSideGreenContainer">
+                    <div id="mainPageWhiteSquare">
+                        <div id="familyPictureContainer">
+                            <img src={FamilyPicture} alt="reunified family picture" title={"reunified family picture"}/>
+                        </div>
+                        <p className="familyPictureText">Family Reunification, for you it is just words for them it is
+                            a <span>reality</span>!</p>
+                    </div>
+                    <div className="greenSquare"></div>
+                </div>
+
+            </div>
+            <div id="lowerSideGreenContainer">
+                <div className="greenRectangle">
+                    <h2>130+</h2>
+                    <span>yet to be Reunified with their families...</span>
+                </div>
+            </div>
+        </div>)
 }
 
 export default App
